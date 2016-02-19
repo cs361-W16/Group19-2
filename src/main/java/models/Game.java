fd9e74d8a1079d7d6592f173d9ba97cf630337ab
Lee Leighton <leel8on@osuosl.org>
2016-02-19 13:04:23 -0800

    Fix more typos
    Fix lines 40-43: period--> comma

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -37,10 +37,10 @@ public class Game {
         else if (isAmerican == false)
         {
             for (int i = 1; i < 11; i++){
-                deck.add(new Card(i.Suit.Coins));
-                deck.add(new Card(i.Suit.Cups));
-                deck.add(new Card(i.Suit.Swords));
-                deck.add(new Card(i.Suit.Clubs));
+                deck.add(new Card(i,Suit.Coins));
+                deck.add(new Card(i,Suit.Cups));
+                deck.add(new Card(i,Suit.Swords));
+                deck.add(new Card(i,Suit.Clubs));
             }
         }
     }
