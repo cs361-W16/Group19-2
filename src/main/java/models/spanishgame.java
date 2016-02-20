05389b5816ba03475843b792efd012ab7d99f0c7
Istallcup <stallcui@oregonstate.edu>
2016-02-20 10:46:57 -0800

    Added a close curly brace to englishgame.java and spanishgame.java...

--- src/main/java/models/spanishgame.java
+++ src/main/java/models/spanishgame.java
@@ -7,14 +7,16 @@ import java.util.Random;
 /**
  * Created by michaelhilton on 1/25/16.
  */
-public class spanishGame extends Game{
+public class spanishGame extends Game {
 
-public void buildDeck() {
- 
-	for (int i = 1; i < 11; ++i){
-		deck.add(new Card(i,Suit.Clubs));
-		deck.add(new Card(i,Suit.Cups));
-		deck.add(new Card(i,Suit.Swords));
-		deck.add(new Card(i,Suit.Coins));
+	public void buildDeck() {
+
+		for (int i = 1; i < 11; ++i) {
+			deck.add(new Card(i, Suit.Clubs));
+			deck.add(new Card(i, Suit.Cups));
+			deck.add(new Card(i, Suit.Swords));
+			deck.add(new Card(i, Suit.Coins));
+		}
 	}
+
 }
