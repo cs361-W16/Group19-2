cb48ca2ebb1a5191d7fa6b2839574a39b498c9b2
Istallcup <stallcui@oregonstate.edu>
2016-02-20 11:07:41 -0800

    Renamed new game files, screwed around with inheritance until build succeeded

---
+++ src/main/java/models/spanishGame.java
@@ -0,0 +1,22 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+/**
+ * Created by michaelhilton on 1/25/16.
+ */
+public class spanishGame extends Game {
+
+	public void buildDeck() {
+
+		for (int i = 1; i < 11; ++i) {
+			deck.add(new Card(i, Suit.Clubs));
+			deck.add(new Card(i, Suit.Cups));
+			deck.add(new Card(i, Suit.Swords));
+			deck.add(new Card(i, Suit.Coins));
+		}
+	}
+
+}
