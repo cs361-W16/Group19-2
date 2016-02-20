2ec26f4044f47a3e4fa77fa16253ada99b02dd33
Aileen <dapper.whale@gmail.com>
2016-02-19 18:48:57 -0800

    Retab b/c the inconsistency bothered me :/

--- src/main/java/models/spanishgame.java
+++ src/main/java/models/spanishgame.java
@@ -10,12 +10,12 @@ import java.util.Random;
 public class spanishGame extends Game{
 
     public void buildDeck() {
-	      for (int i = 1; i < 11; ++i){
-	          deck.add(new Card(i,Suit.Clubs));
-		        deck.add(new Card(i,Suit.Cups));
-		        deck.add(new Card(i,Suit.Swords));
-		        deck.add(new Card(i,Suit.Coins));
-	      }
+        for (int i = 1; i < 11; ++i){
+            deck.add(new Card(i,Suit.Clubs));
+            deck.add(new Card(i,Suit.Cups));
+            deck.add(new Card(i,Suit.Swords));
+            deck.add(new Card(i,Suit.Coins));
+        }
     }
 
 }
--- src/test/java/models/testspanishGame.java
+++ src/test/java/models/testspanishGame.java
@@ -6,16 +6,16 @@ import static org.junit.Assert.*;
 
 public class testSpanishGame{
 
-	@Test
-	public void  testSpanishGameCreation(){
-		Game s = new spanishGame();
-		assertNotNull(s);
-	}
+  @Test
+  public void  testSpanishGameCreation(){
+    Game s = new spanishGame();
+    assertNotNull(s);
+  }
 
-	@Test
+  @Test
   public void testSpanishDeckCount(){
-	  Game s = new spanishGame();
-	  s.buildDeck();
+    Game s = new spanishGame();
+    s.buildDeck();
     assertEquals(40, s.deck.size()); 
   }
 
