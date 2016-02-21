c38301802e2d86c310cc8204e7360c64ed4ed969
Lee Leighton <leightle@oregonstate.edu>
2016-02-20 19:13:23 -0800

    Add athai's tests

---
+++ src/test/java/models/testEnglishGame.java
@@ -0,0 +1,22 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class testEnglishGame{
+
+    @Test
+    public void testEnglishGameCreation(){
+        Game s = new englishGame();
+        assertNotNull(s);
+    }
+
+    @Test
+    public void testEnglishDeckBuilt(){
+        Game s = new englishGame();
+        s.buildDeck();
+        assertEquals(52, s.deck.size());
+    }
+
+}
